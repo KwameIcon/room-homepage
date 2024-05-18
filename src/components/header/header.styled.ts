887330ef4798e1f5f1193f9e45fe2558b0bdf9bf
Kwameicon <kwameicon.gh.bs@gmail.com>
2024-05-18 12:09:42 +0000

    fix: add menu toggling functionality and correct prop types

--- src/components/header/header.styled.ts
+++ src/components/header/header.styled.ts
@@ -1,53 +1,65 @@
 import styled from "styled-components";
 
 type displayMenuProps = {
-    displayMenu: boolean,
+  displayMenu: boolean;
 }
 
 export const StyledHeader = styled.div<displayMenuProps>`
-    position: fixed;
-    width: 362px;
-    height: 15px;
-    left: 0px;
-    top: 63px;
-    z-index: 10;
-    background: transparent;
-    display: flex;
-    gap: 20px;
-    justify-content: space-evenly;
-    align-items: center;
+  position: fixed;
+  width: 362px;
+  height: 15px;
+  left: 0px;
+  top: 63px;
+  z-index: 10;
+  background: transparent;
+  display: flex;
+  gap: 20px;
+  justify-content: space-evenly;
+  align-items: center;
 
-    & > img:first-child{
-        display: none;
+  @media screen and (max-width: 375px) {
+    width: 100vw;
 
-        @media screen and (max-width: 375px) {
-            display: block;
-            position: absolute;
-            width: 20px;
-            height: 14px;
-            left: 24px;
-            top: 0px;
-            color: #000;
-            cursor: pointer;
-        }
+
+    .overlay{
+        position: fixed;
+        inset: 0;
+        width: 100%;
+        height: 1538px;
+        background: rgba(0, 0, 0, 0.4);
+        z-index: 10;
+        visibility: ${({ displayMenu }) => displayMenu ? 'visible' : 'hidden'};
     }
+  }
 
-    & > :nth-child(2){
-        width: 61.72px;
-        height: 13.06px;
+  & > img:nth-child(2) {
+    display: none;
 
-        @media screen and (max-width: 375px) {
-            width: 61.72px;
-            height: 13.06px;
-        }
+    @media screen and (max-width: 375px) {
+      display: block;
+      position: absolute;
+      width: 20px;
+      height: 14px;
+      left: 24px;
+      top: 0px;
+      color: #000;
+      cursor: pointer;
     }
+  }
 
-    & > :last-child{
-        
-        @media screen and (max-width: 375px) {
-            display: ${({displayMenu}) => displayMenu ? 'block' : 'none'};
-        }
+  & > :nth-child(3) {
+    width: 61.72px;
+    height: 13.06px;
+
+    @media screen and (max-width: 375px) {
+      width: 61.72px;
+      height: 13.06px;
     }
+  }
 
-
-`
+  & > :last-child {
+    @media screen and (max-width: 375px) {
+      visibility: ${({ displayMenu }) => displayMenu ? 'visible' : 'hidden'};
+    }
+  }
+`;
